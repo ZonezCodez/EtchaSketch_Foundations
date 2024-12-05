efa9146f6c0b3ae76dbb3a2a12af1cc5a03fcc35
Zone_Codez <odincodezone@gmail.com>
2024-12-05 09:40:41 -0600

    finished current etch a sketch added ability to choose board size

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ let cols = 16;
 let color = ['red','green','blue','yellow','orange','black','purple'];
 // Variable for the board
 let gameBoard = document.getElementById('game');
+//Variable for the change size button
+let sizeBtn = document.getElementById('size');
+// Add event listener for the button 
+sizeBtn.addEventListener('click',(e) => {
+    getnewSize();
+})
+// Function to get new size board 
+function getnewSize(){
+    r = prompt('How many rows?');
+    rows = parseInt(r);
+    c = prompt('How many columns?');
+    cols = parseInt(c);
+    if(rows > 100 || cols > 100 || rows < 2 || cols < 2){
+        return getnewSize();
+    }
+    return createGrid(rows,cols);
+}
+// Function to refresh board to make a new one.
+function clearGrid(){
+    gameBoard.textContent = "";
+}
 // Function to create the drawing board
 function createGrid(rows,cols){
+    clearGrid();
     for(let e = 0; e < rows*cols;e++){
         let div = document.createElement('div'); 
         div.className = 'boardPieces';
